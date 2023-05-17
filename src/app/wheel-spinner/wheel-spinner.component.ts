648b5fa1cd5de7f6da613b34ed2eb8667c9eaeea
WestwindGroupLLC <info@westwindgroupllc.com>
2023-05-17 12:04:58 -0400

    kickstart 3

--- src/app/wheel-spinner/wheel-spinner.component.ts
+++ src/app/wheel-spinner/wheel-spinner.component.ts
@@ -7,51 +7,51 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class WheelSpinnerComponent {
   @Input() options: string[] = [];
-  @Output() spinCompleted: EventEmitter<string> = new EventEmitter<string>();
+@Output() spinCompleted: EventEmitter<string> = new EventEmitter<string>();
 
-  selectedOption: string | undefined;
-  isSpinning: boolean = false;
-  rotationAngle: number = 0;
+selectedOption: string | undefined;
+isSpinning: boolean = false;
+rotationAngle: number = 0;
 
-  spinWheel() {
-    if (this.options.length > 0) {
-      this.isSpinning = true;
-      const randomIndex = Math.floor(Math.random() * this.options.length);
-      this.selectedOption = this.options[randomIndex];
-      this.animateSpin();
-
-      setTimeout(() => {
-        this.isSpinning = false;
-        this.spinCompleted.emit(this.selectedOption!);
-      }, 3000);
-    }
-  }
-
-  animateSpin() {
-    const spinInterval = setInterval(() => {
-      this.rotationAngle += 10;
-      if (this.rotationAngle >= 360) {
-        this.rotationAngle = 0;
-      }
-    }, 50);
+spinWheel() {
+  if (this.options.length > 0) {
+    this.isSpinning = true;
+    const randomIndex = Math.floor(Math.random() * this.options.length);
+    this.selectedOption = this.options[randomIndex];
+    this.animateSpin();
 
     setTimeout(() => {
-      clearInterval(spinInterval);
+      this.isSpinning = false;
+      this.spinCompleted.emit(this.selectedOption!);
     }, 3000);
   }
-
-  respinWheel() {
-    if (!this.isSpinning) {
-      this.selectedOption = undefined;
+}
+animateSpin() {
+  const spinInterval = setInterval(() => {
+    this.rotationAngle += 10;
+    if (this.rotationAngle >= 360) {
+      this.rotationAngle = 0;
     }
-  }
+  }, 50);
 
-  resetWheel() {
+  setTimeout(() => {
+    clearInterval(spinInterval);
+  }, 3000);
+}
+
+respinWheel() {
+  if (!this.isSpinning) {
     this.selectedOption = undefined;
   }
+}
 
-  getSegmentColor(option: string): string {
-    // Add your logic to return the desired color for each segment
-    return 'red';
-  }
+resetWheel() {
+  this.selectedOption = undefined;
+}
+
+getSegmentColor(option: string): string {
+  // Add your logic to return the desired color for each segment
+  return 'red';
+}
+
 }
