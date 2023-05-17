d9668f18647f9aaf468200973a0c5c7141fff8a0
WestwindGroupLLC <info@westwindgroupllc.com>
2023-05-16 21:41:01 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Wheel-of-Golf',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
